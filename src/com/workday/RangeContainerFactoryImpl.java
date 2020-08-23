56b34b8cd9a1440d94f994306dd0cd24c5e02c22
Karan Warrier <kawarrier@yahoo.com>
2020-08-23 00:39:46 -0700

    remove print statement

--- src/com/workday/RangeContainerFactoryImpl.java
+++ src/com/workday/RangeContainerFactoryImpl.java
@@ -4,14 +4,9 @@ import java.util.Arrays;
 
 public class RangeContainerFactoryImpl implements RangeContainerFactory {
 
-    public RangeContainerFactoryImpl() {
-
-    }
+    public RangeContainerFactoryImpl() {}
 
     public RangeContainer createContainer(long[] data) {
-        for (long d: data) {
-            System.out.print(d+" ");
-        }
         RangeContainer container = new RangeContainerImpl(data);
         return container;
     }
