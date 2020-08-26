1b4352d98ea635140d871c1f62228203d39f1528
Karan Warrier <kawarrier@yahoo.com>
2020-08-26 00:28:05 -0700

    add some comments

--- src/com/workday/RangeContainerImpl.java
+++ src/com/workday/RangeContainerImpl.java
@@ -26,6 +26,8 @@ public class RangeContainerImpl implements RangeContainer {
         Queue<Integer> que = new LinkedList<>();
         List<Integer> lst = Collections.synchronizedList(new ArrayList());
 
+        // needed to use this to await for all threads that were spun up to then terminate.
+        // Once all threads are terminated, then it is known the full dataset has been passed through
         ExecutorService es = Executors.newCachedThreadPool();
         boolean finished = false;
         for(int i=0;i<partitions.size();i++) {
@@ -45,6 +47,7 @@ public class RangeContainerImpl implements RangeContainer {
             System.out.println(e);
         }
 
+        // sort the indices and add them to the queue
         if (finished) {
             Collections.sort(lst);
             for (int n: lst) {
@@ -57,6 +60,12 @@ public class RangeContainerImpl implements RangeContainer {
         return result;
     }
 
+    /**
+     *
+     * @param data
+     * @param numWorkers
+     * @return data evenly distributed through partitions, though the original order of indices is not preserved
+     */
     private List<List<long[]>> partitionData(long[] data, int numWorkers) {
         List<List<long[]>> partitions = new ArrayList<>();
         for (int i = 0; i < numWorkers; i++) {
@@ -76,6 +85,10 @@ public class RangeContainerImpl implements RangeContainer {
         return partitions;
     }
 
+    /**
+     *
+     * Adds all the elements that meet the range criteria to the list being updated
+     */
     private void addElemToQueue(List<long[]> partition, long lowerBound, long upperBound, boolean fromInclusive, boolean toInclusive, List<Integer> lst) {
         for (int i = 0; i < partition.size(); i++) {
             long[] pair = partition.get(i);
